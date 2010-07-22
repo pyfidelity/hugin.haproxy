8fb2972bc8950766917ad9cc866ae18fca210d5f
Matthew Wilkes <git@matthewwilkes.name>
2010-07-22 15:17:08 +0000

    Initial work on summary CSVs and tests
    git-svn-id: file:///Users/fschulze/temp/svn/jarn/public/hugin.haproxy/trunk@37437 dd099063-dbd4-0310-9226-844929cde8ce

---
+++ hugin/haproxy/goals.py
@@ -0,0 +1,94 @@
+from csv import DictWriter
+import datetime
+import itertools
+import os
+
+from hugin.haproxy.filters.timingstats import TimingStatistics
+from hugin.haproxy.logparser import logparser, DATE_FORMAT
+
+class GoalAnalyser(object):
+    """Takes a log file and some filters for URL specific stats and generates 
+    CSV files with the result of the TimingStatistics filter"""
+    
+    def __init__(self, log, location, urls={}):
+        super(GoalAnalyser, self).__init__()
+        self.log = log
+        self.urls = urls
+        self.dir = location
+        self._statscounters = {}
+        self._outputs = {}
+        self._files = {}
+        
+        self.parse = logparser()
+        
+        self._instantiateFilters()
+        self._instantiateCSVWriters()
+
+    def _instantiateFilters(self):
+        for name in self.urls:
+            self._statscounters[name] = TimingStatistics() 
+
+    def _instantiateCSVWriters(self):
+        for name in self.urls:
+            location = os.path.join(self.dir, '%s_stats.csv' % name)
+            backing = open(location, 'w')
+            self._files[name] = backing
+            
+            keys = ['date', ] + TimingStatistics().stats().keys()
+            
+            writer = DictWriter(backing, keys)
+            writer.writerow(dict(zip(keys, keys)))
+            
+            self._outputs[name] = writer
+
+    @property
+    def filters(self):
+        return self.urls.keys()
+
+    @property
+    def statscounters(self):
+        return self._statscounters
+    
+    @property
+    def outputs(self):
+        return self._outputs
+    
+    def filterForLine(self, line):
+        self.getDateForLine(line)
+        for name in self.statscounters.keys():
+            condition = self.urls[name]
+            if condition[1].match(line['url']) and condition[0] == line['method']:
+                return name
+    
+    def getDateForLine(self, line):
+        date = datetime.datetime.strptime(line['date'], DATE_FORMAT)
+        return date.date()
+    
+    def __call__(self):
+        iterable = itertools.imap(self.parse, self.log)
+        
+        days = itertools.groupby(iterable, self.getDateForLine)
+        
+        for day, iterable in days:
+            parsed, destination = itertools.tee(iterable)
+            destination = itertools.imap(self.filterForLine, destination)
+        
+            classified = itertools.izip(destination, parsed)
+        
+            for destination, entry in classified:
+                self.statscounters[destination].process(entry)
+        
+            for name in self.filters:
+                stats = self.statscounters[name].stats()
+                self.outputs[name].writerow(stats)
+            
+        self.finish()
+    
+    def finish(self):
+        for f in self._files.values():
+            f.flush()
+            os.fsync(f.fileno())
+            f.close()
+        
+        
+        
---
+++ hugin/haproxy/tests/test_target_stats.py
@@ -0,0 +1,65 @@
+import re
+import unittest
+import tempfile
+from io import BytesIO
+import os
+
+from hugin.haproxy.goals import GoalAnalyser
+
+SAMPLE_LOG = """Jul 21 17:25:59 127.0.0.1 haproxy[2474]: 127.0.0.1:49275 [21/Jul/2010:17:25:59.434] zopecluster zope/backend 0/0/0/395/396 200 3535 - - ---- 0/0/0/0/0 0/0 "GET /VirtualHostBase/http/www.site.example:80/subsite/VirtualHostRoot/ HTTP/1.0" """
+
+class TestConfiguration(unittest.TestCase):
+
+    def setUp(self):
+        configs = { 'home':('GET', re.compile("^/?$")), }
+        
+        self.analyser = GoalAnalyser(BytesIO(SAMPLE_LOG), location=tempfile.gettempdir(), urls=configs)
+
+    def test_stats_counter_instantiated(self):
+        self.assertEqual(len(self.analyser.statscounters), 1)
+        self.assertEqual(self.analyser.statscounters.keys(), ["home"])
+
+    def test_sample_filtered_to_home(self):
+        parsed = self.analyser.parse(SAMPLE_LOG)
+        self.assertEqual(self.analyser.filterForLine(parsed), 'home')
+    
+    def test_running_dumps_into_output(self):
+        self.analyser()
+        location = os.path.join(tempfile.gettempdir(), 'home_stats.csv')
+        output = open(location, 'r').readlines()
+        self.assertEqual(len(output), 2) # Header row and one day
+
+LONGER_LOG = """Jul 21 17:25:59 127.0.0.1 haproxy[2474]: 127.0.0.1:49275 [21/Jul/2010:17:25:59.434] zopecluster zope/backend 0/0/0/395/396 200 3535 - - ---- 0/0/0/0/0 0/0 "GET /VirtualHostBase/http/www.site.example:80/subsite/VirtualHostRoot/ HTTP/1.0" 
+Jul 22 17:25:59 127.0.0.1 haproxy[2474]: 127.0.0.1:49275 [22/Jul/2010:17:25:59.434] zopecluster zope/backend 0/0/0/395/396 200 3535 - - ---- 0/0/0/0/0 0/0 "GET /VirtualHostBase/http/www.site.example:80/subsite/VirtualHostRoot/ HTTP/1.0" 
+Jul 23 17:25:59 127.0.0.1 haproxy[2474]: 127.0.0.1:49275 [23/Jul/2010:17:25:59.434] zopecluster zope/backend 0/0/0/395/396 200 3535 - - ---- 0/0/0/0/0 0/0 "GET /VirtualHostBase/http/www.site.example:80/subsite/VirtualHostRoot/ HTTP/1.0" 
+Jul 24 17:25:59 127.0.0.1 haproxy[2474]: 127.0.0.1:49275 [24/Jul/2010:17:25:59.434] zopecluster zope/backend 0/0/0/395/396 200 3535 - - ---- 0/0/0/0/0 0/0 "GET /VirtualHostBase/http/www.site.example:80/subsite/VirtualHostRoot/ HTTP/1.0" """
+
+MULTI_ENTRY_LOG = """Jul 21 17:25:59 127.0.0.1 haproxy[2474]: 127.0.0.1:49275 [21/Jul/2010:17:25:59.434] zopecluster zope/backend 0/0/0/122/122 200 3535 - - ---- 0/0/0/0/0 0/0 "GET /VirtualHostBase/http/www.site.example:80/subsite/VirtualHostRoot/ HTTP/1.0" 
+Jul 21 17:25:59 127.0.0.1 haproxy[2474]: 127.0.0.1:49275 [21/Jul/2010:17:25:59.434] zopecluster zope/backend 0/0/0/395/396 200 3535 - - ---- 0/0/0/0/0 0/0 "GET /VirtualHostBase/http/www.site.example:80/subsite/VirtualHostRoot/ HTTP/1.0"
+Jul 21 17:25:59 127.0.0.1 haproxy[2474]: 127.0.0.1:49275 [21/Jul/2010:17:25:59.434] zopecluster zope/backend 0/0/0/331/331 200 3535 - - ---- 0/0/0/0/0 0/0 "GET /VirtualHostBase/http/www.site.example:80/subsite/VirtualHostRoot/ HTTP/1.0"
+Jul 22 17:25:59 127.0.0.1 haproxy[2474]: 127.0.0.1:49275 [22/Jul/2010:17:25:59.434] zopecluster zope/backend 0/0/0/395/396 200 3535 - - ---- 0/0/0/0/0 0/0 "GET /VirtualHostBase/http/www.site.example:80/subsite/VirtualHostRoot/ HTTP/1.0" 
+Jul 22 17:25:59 127.0.0.1 haproxy[2474]: 127.0.0.1:49275 [22/Jul/2010:17:25:59.434] zopecluster zope/backend 0/0/0/91/94 200 3535 - - ---- 0/0/0/0/0 0/0 "GET /VirtualHostBase/http/www.site.example:80/subsite/VirtualHostRoot/ HTTP/1.0" 
+Jul 23 17:25:59 127.0.0.1 haproxy[2474]: 127.0.0.1:49275 [23/Jul/2010:17:25:59.434] zopecluster zope/backend 0/0/0/395/396 200 3535 - - ---- 0/0/0/0/0 0/0 "GET /VirtualHostBase/http/www.site.example:80/subsite/VirtualHostRoot/ HTTP/1.0" 
+Jul 24 17:25:59 127.0.0.1 haproxy[2474]: 127.0.0.1:49275 [24/Jul/2010:17:25:59.434] zopecluster zope/backend 0/0/0/1212/1212 200 3535 - - ---- 0/0/0/0/0 0/0 "GET /VirtualHostBase/http/www.site.example:80/subsite/VirtualHostRoot/ HTTP/1.0" """
+
+
+class TestSummary(unittest.TestCase):
+
+    def setUp(self):
+        self.configs = { 'home':('GET', re.compile("^/?$")), }
+
+    def test_multiple_days_have_multiple_rows(self):
+        self.analyser = GoalAnalyser(BytesIO(LONGER_LOG), location=tempfile.gettempdir(), urls=self.configs)
+        
+        self.analyser()
+        location = os.path.join(tempfile.gettempdir(), 'home_stats.csv')
+        output = open(location, 'r').readlines()
+        self.assertEqual(len(output), 5) # Header row and one day
+    
+    def test_data_for_one_day_is_combined_into_aggregates(self):
+        self.analyser = GoalAnalyser(BytesIO(MULTI_ENTRY_LOG), location=tempfile.gettempdir(), urls=self.configs)
+
+        self.analyser()
+        location = os.path.join(tempfile.gettempdir(), 'home_stats.csv')
+        output = open(location, 'r').readlines()
+        self.assertEqual(len(output), 5) # Header row and one day
