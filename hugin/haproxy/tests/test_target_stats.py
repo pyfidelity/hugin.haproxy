b0739acc00c71967e76d430834913f0e8e445e96
Matthew Wilkes <git@matthewwilkes.name>
2010-07-26 09:55:44 +0000

    Switch to using mkdtemp for temporary dirs so csvs do not leak between test runs.
    git-svn-id: file:///Users/fschulze/temp/svn/jarn/public/hugin.haproxy/trunk@37468 dd099063-dbd4-0310-9226-844929cde8ce

--- hugin/haproxy/tests/test_target_stats.py
+++ hugin/haproxy/tests/test_target_stats.py
@@ -1,8 +1,10 @@
+from csv import DictReader
 import re
 import unittest
 import tempfile
 from io import BytesIO
 import os
+import shutil
 
 from hugin.haproxy.goals import GoalAnalyser
 
@@ -30,12 +32,23 @@ Jul 22 17:25:59 127.0.0.1 haproxy[2474]: 127.0.0.1:49275 [22/Jul/2010:17:25:59.4
 Jul 23 17:25:59 127.0.0.1 haproxy[2474]: 127.0.0.1:49275 [23/Jul/2010:17:25:59.434] zopecluster zope/backend 0/0/0/395/396 200 3535 - - ---- 0/0/0/0/0 0/0 "GET /VirtualHostBase/http/www.site.example:80/subsite/VirtualHostRoot/ HTTP/1.0" 
 Jul 24 17:25:59 127.0.0.1 haproxy[2474]: 127.0.0.1:49275 [24/Jul/2010:17:25:59.434] zopecluster zope/backend 0/0/0/1212/1212 200 3535 - - ---- 0/0/0/0/0 0/0 "GET /VirtualHostBase/http/www.site.example:80/subsite/VirtualHostRoot/ HTTP/1.0" """
 
-class TestSimpleConfiguration(unittest.TestCase):
+
+class TempdirAvailable(unittest.TestCase):
+    
+    def setUp(self):
+        self.location = tempfile.mkdtemp()
+    
+    def tearDown(self):
+        shutil.rmtree(self.location)
+
+
+class TestSimpleConfiguration(TempdirAvailable):
 
     def setUp(self):
+        TempdirAvailable.setUp(self)
         configs = { 'home':('GET', re.compile("^/?$")), }
         
-        self.analyser = GoalAnalyser(BytesIO(SAMPLE_LOG), location=tempfile.gettempdir(), urls=configs)
+        self.analyser = GoalAnalyser(BytesIO(SAMPLE_LOG), location=self.location, urls=configs)
 
     def test_stats_counter_instantiated(self):
         self.assertEqual(len(self.analyser.statscounters), 1)
@@ -47,12 +60,12 @@ class TestSimpleConfiguration(unittest.TestCase):
     
     def test_running_dumps_into_output(self):
         self.analyser()
-        location = os.path.join(tempfile.gettempdir(), 'home_stats.csv')
+        location = os.path.join(self.location, 'home_stats.csv')
         output = open(location, 'r').readlines()
         self.assertEqual(len(output), 2) # Header row and one day
     
     def test_historical_data_is_left_intact(self):
-        location = os.path.join(tempfile.gettempdir(), 'home_stats.csv')
+        location = os.path.join(self.location, 'home_stats.csv')
         prep = open(location, 'w')
         prep.write("""date,median,ninety,stddev,ten,max,avg,eighty
 2009-01-01,331,396,116.895965143,122,396,283,396
@@ -62,41 +75,56 @@ class TestSimpleConfiguration(unittest.TestCase):
         output = open(location, 'r').readlines()
         self.assertEqual(len(output), 4) # Header row, two historical, one new
     
+    def test_csv_is_in_a_valid_format(self):
+        self.analyser()
 
-class TestInvalidLogEntry(unittest.TestCase):
+        location = os.path.join(self.location, 'home_stats.csv')
+        prep = open(location, 'r')
+        csv = DictReader(prep)
+        self.failUnless(len(list(csv)), 1)
+
+    
+
+class TestInvalidLogEntry(TempdirAvailable):
     
     def setUp(self):
+        TempdirAvailable.setUp(self)
+
         configs = { 'home':('GET', re.compile("^/?$")), }
         
-        self.analyser = GoalAnalyser(BytesIO(INVALID_LOG), location=tempfile.gettempdir(), urls=configs)
+        self.analyser = GoalAnalyser(BytesIO(INVALID_LOG), location=self.location, urls=configs)
     
     def test_empty_output_without_errors(self):
         self.analyser()
-        location = os.path.join(tempfile.gettempdir(), 'home_stats.csv')
+        location = os.path.join(self.location, 'home_stats.csv')
         output = open(location, 'r').readlines()
         self.assertEqual(len(output), 2) # There is a single valid line at the start of the input
 
-class TestUnknownLogEntry(unittest.TestCase):
+class TestUnknownLogEntry(TempdirAvailable):
     
     def setUp(self):
+        TempdirAvailable.setUp(self)
+
         configs = { 'home':('GET', re.compile("^/?$")), }
         
-        self.analyser = GoalAnalyser(BytesIO(BOGUS_LOG), location=tempfile.gettempdir(), urls=configs)
+        self.analyser = GoalAnalyser(BytesIO(BOGUS_LOG), location=self.location, urls=configs)
     
     def test_empty_output_without_errors(self):
         self.analyser()
-        location = os.path.join(tempfile.gettempdir(), 'home_stats.csv')
+        location = os.path.join(self.location, 'home_stats.csv')
         output = open(location, 'r').readlines()
         self.assertEqual(len(output), 1)
 
-class TestMultipleTargetsInConfiguration(unittest.TestCase):
+class TestMultipleTargetsInConfiguration(TempdirAvailable):
 
     def setUp(self):
+        TempdirAvailable.setUp(self)
+
         configs = { 'home':('GET', re.compile("^/?$")), 
                     'news':('GET', re.compile("^/news")), 
                     'register':('POST', re.compile("^/register$")), }
         
-        self.analyser = GoalAnalyser(BytesIO(SAMPLE_LOG), location=tempfile.gettempdir(), urls=configs)
+        self.analyser = GoalAnalyser(BytesIO(SAMPLE_LOG), location=self.location, urls=configs)
 
     def test_all_counters_initialised(self):
         self.assertEqual(len(self.analyser.statscounters), 3)
@@ -108,35 +136,37 @@ class TestMultipleTargetsInConfiguration(unittest.TestCase):
     
     def test_running_dumps_into_output(self):
         self.analyser()
-        location = os.path.join(tempfile.gettempdir(), 'home_stats.csv')
+        location = os.path.join(self.location, 'home_stats.csv')
         output = open(location, 'r').readlines()
         self.assertEqual(len(output), 2) # Header row and one day
         
-        location = os.path.join(tempfile.gettempdir(), 'register_stats.csv')
+        location = os.path.join(self.location, 'register_stats.csv')
         output = open(location, 'r').readlines()
         self.assertEqual(len(output), 1) # Header row only
          
-        location = os.path.join(tempfile.gettempdir(), 'news_stats.csv')
+        location = os.path.join(self.location, 'news_stats.csv')
         output = open(location, 'r').readlines()
         self.assertEqual(len(output), 1) # Header row only
 
-class TestSummary(unittest.TestCase):
+class TestSummary(TempdirAvailable):
 
     def setUp(self):
+        TempdirAvailable.setUp(self)
+
         self.configs = { 'home':('GET', re.compile("^/?$")), }
 
     def test_multiple_days_have_multiple_rows(self):
-        self.analyser = GoalAnalyser(BytesIO(LONGER_LOG), location=tempfile.gettempdir(), urls=self.configs)
+        self.analyser = GoalAnalyser(BytesIO(LONGER_LOG), location=self.location, urls=self.configs)
         
         self.analyser()
-        location = os.path.join(tempfile.gettempdir(), 'home_stats.csv')
+        location = os.path.join(self.location, 'home_stats.csv')
         output = open(location, 'r').readlines()
         self.assertEqual(len(output), 5) # Header row and one day
     
     def test_data_for_one_day_is_combined_into_aggregates(self):
-        self.analyser = GoalAnalyser(BytesIO(MULTI_ENTRY_LOG), location=tempfile.gettempdir(), urls=self.configs)
+        self.analyser = GoalAnalyser(BytesIO(MULTI_ENTRY_LOG), location=self.location, urls=self.configs)
 
         self.analyser()
-        location = os.path.join(tempfile.gettempdir(), 'home_stats.csv')
+        location = os.path.join(self.location, 'home_stats.csv')
         output = open(location, 'r').readlines()
         self.assertEqual(len(output), 5) # Header row and one day
