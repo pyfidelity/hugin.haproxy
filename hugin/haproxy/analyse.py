22739b31a450c97f34353f72afd9d9b32d912ed3
Andreas Zeidler <az@zitc.de>
2010-07-27 10:34:41 +0000

    update for r37504: let's rather output the list of graphs in csv format (and parse that via javascript), so we can keep markup, js, css etc in on place (i.e. outside `hugin.haproxy`) and separate from the code
    git-svn-id: file:///Users/fschulze/temp/svn/jarn/public/hugin.haproxy/trunk@37505 dd099063-dbd4-0310-9226-844929cde8ce

--- hugin/haproxy/analyse.py
+++ hugin/haproxy/analyse.py
@@ -2,8 +2,9 @@ from hugin.haproxy.goals import GoalAnalyser
 from hugin.haproxy.configuration import FilterConfig
 from argparse import ArgumentParser
 from fileinput import input, hook_compressed
-from re import compile
-import os
+from csv import writer
+from os.path import join
+
 
 def main():
     parser = ArgumentParser(description='Analyse haproxy logs.')
@@ -16,23 +17,18 @@ def main():
     config.read(args.config)
     urls = config.urls()
 
-    analyser = GoalAnalyser(input(args.log, openhook=hook_compressed), location=args.directory, urls=urls)
+    analyser = GoalAnalyser(input(args.log, openhook=hook_compressed),
+        location=args.directory, urls=urls)
     analyser()
-    
-    with open(os.path.join(args.directory, "index.html"), "w") as html_out:
-        html_out.write(generateHTML(config))
 
-SECTION = """<h3>%(title)s</h3><div id="%(id)s" class="chart maxval-%(max)d"></div><br />"""
+    csvname = join(args.directory, 'index.csv')
+    generateStatsIndex(open(csvname, 'wb'), config)
 
-def generateHTML(config):
-    location = os.path.join(os.path.dirname(__file__), "stats_template.html")
-    with open(location, "r") as f:
-        template = f.read()
-        
-    charts = ''
 
-    for section, items in config._sections.items():
-        vals = {"id":section, "title":items.get('title', section), "max":int(items.get('max', "3000"))}
-        charts += SECTION % vals
-    
-    return template % (charts)
+def generateStatsIndex(output, config):
+    csv = writer(output)
+    csv.writerow(('section', 'title', 'limit'))
+    for section in config.sections():
+        items = dict(config.items(section))
+        csv.writerow((section, items.get('title', section),
+            int(items.get('max', 3000))))
---
+++ hugin/haproxy/tests/test_stats_index.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+from io import BytesIO
+from hugin.haproxy.analyse import generateStatsIndex
+from hugin.haproxy.configuration import FilterConfig
+
+
+config = """
+[home]
+method = GET
+match = ^/$
+max = 2000
+title = Home page
+
+[about]
+method = GET
+match = ^/about$
+"""
+
+
+class TestGenerateHTML(TestCase):
+
+    def setUp(self):
+        self.config = FilterConfig()
+        self.config.readfp(BytesIO(config))
+
+    def test_index_generated_for_rule(self):
+        output = BytesIO()
+        generateStatsIndex(output, self.config)
+        self.assertEqual(output.getvalue().splitlines(), [
+            'section,title,limit',
+            'home,Home page,2000',
+            'about,about,3000',
+        ])
